565a82987d0e47743f136348076d1d21a8c83708
perrycpf <perry1629@gmail.com>
2019-03-22 19:59:09 -0400

    update files

--- client/src/views/WageMap.jsx
+++ client/src/views/WageMap.jsx
@@ -1,13 +1,25 @@
-import React from 'react'
-import mapImage from '../wagemap.jpg'
+import React, { Component } from 'react';
+// import './App.css';
 
-const WageMap = (props) => {
-	return (
-		<div className='Wage-Map'>
-			<h1>Welcome to the Wage Visualizer!</h1>
-			<img src={mapImage} alt="Wage Map" />
-		</div>
-	)
+import ChoroplethMap from './ChoroplethMap';
+
+class App extends Component {
+  state = {
+    data: [
+      ["MB", 75], ["SK", 43], ["AB", 50], ["BC", 88], ["NU", 21], ["NT", 43],
+      ["YT", 21], ["ON", 19], ["QC", 60], ["NB", 4], ["NS", 44], ["NF", 38],
+      ["PE", 67]],
+  }
+  render() {
+    return (
+      <div style={{
+        height:"100vh",
+        width: "100vw"
+      }}>
+        <ChoroplethMap data={this.state.data}/>
+      </div>
+    );
+  }
 }
 
-export default WageMap
+export default App;
